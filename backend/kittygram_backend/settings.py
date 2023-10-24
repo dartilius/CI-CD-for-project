fa2bf993c6b0e5f31c92cbf868cd51ef36ed046b
borisovi <borisovi@krasrm.com>
2023-10-24 14:58:46 +0700

    add key and hosts

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -3,11 +3,11 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY', 'empty')
 
 DEBUG = os.getenv('DEBUG', 'False').lower() == 'true'
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(', ')
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(', ')
 
 
 INSTALLED_APPS = [
