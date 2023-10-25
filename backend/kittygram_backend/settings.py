1db643a118d8df6fae0b65bfeb159d915648b49e
borisovi <borisovi@krasrm.com>
2023-10-25 16:05:39 +0700

    fix first review

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.getenv('SECRET_KEY', 'empty')
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 DEBUG = os.getenv('DEBUG', 'False').lower() == 'true'
 
